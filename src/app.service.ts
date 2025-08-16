85dbf110b988f444cf08ea9965bf6b851b53758e
amazingawwal <sakeebmawwal@gmail.com>
2025-08-16 22:29:37 +0100

    updated base modules (controller & service)

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  greetings(): string {
+    return 'Hello, Welcome to my Blog!';
   }
 }
