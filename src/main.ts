254424305a7e77ace82f3d86cf2eeed09651fc55
amazingawwal <sakeebmawwal@gmail.com>
2025-08-31 23:04:18 +0100

    docs: sessions

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
+
+
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    session({
+      secret: 'my-secret-key', // change this in production!
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 60000 }, // 1 minute for testing
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
